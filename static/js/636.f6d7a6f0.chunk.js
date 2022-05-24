db96aa149686e1ef6ea777257b8e1e8db45710e1
bbecquet <benjamin.becquet@gmail.com>
2022-05-24 06:53:50 +0000

    Deploying to gh-pages from @ bbecquet/bird-tracking@cdb48a0495b3d0137faeb678f65aaeb82b7ba53e 🚀

---
+++ static/js/636.f6d7a6f0.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunkbird_tracking_map=self.webpackChunkbird_tracking_map||[]).push([[636],{636:function(a,e){e.printMsg=function(){console.log("This is a message from the demo package")}}}]);
+//# sourceMappingURL=636.f6d7a6f0.chunk.js.map
